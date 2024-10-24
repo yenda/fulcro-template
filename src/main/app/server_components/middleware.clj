de45a959b2b154852bf58fb7b871cd0f0e0ad394
Eric Dvorsak <yenda1@gmail.com>
2024-10-24 10:58:44 +0200

    quill editor issue repro

--- src/main/app/server_components/middleware.clj
+++ src/main/app/server_components/middleware.clj
@@ -1,16 +1,16 @@
 (ns app.server-components.middleware
   (:require
-    [app.server-components.config :refer [config]]
-    [app.server-components.pathom :refer [parser]]
-    [mount.core :refer [defstate]]
-    [com.fulcrologic.fulcro.server.api-middleware :refer [handle-api-request
-                                                          wrap-transit-params
-                                                          wrap-transit-response]]
-    [ring.middleware.defaults :refer [wrap-defaults]]
-    [ring.util.response :refer [response file-response resource-response]]
-    [ring.util.response :as resp]
-    [hiccup.page :refer [html5]]
-    [taoensso.timbre :as log]))
+   [app.server-components.config :refer [config]]
+   [app.server-components.pathom :refer [parser]]
+   [mount.core :refer [defstate]]
+   [com.fulcrologic.fulcro.server.api-middleware :refer [handle-api-request
+                                                         wrap-transit-params
+                                                         wrap-transit-response]]
+   [ring.middleware.defaults :refer [wrap-defaults]]
+   [ring.util.response :refer [response file-response resource-response]]
+   [ring.util.response :as resp]
+   [hiccup.page :refer [html5]]
+   [taoensso.timbre :as log]))
 
 (def ^:private not-found-handler
   (fn [req]
@@ -18,13 +18,12 @@
      :headers {"Content-Type" "text/plain"}
      :body    "NOPE"}))
 
-
 (defn wrap-api [handler uri]
   (fn [request]
     (if (= uri (:uri request))
       (handle-api-request
-        (:transit-params request)
-        (fn [tx] (parser {:ring/request request} tx)))
+       (:transit-params request)
+       (fn [tx] (parser {:ring/request request} tx)))
       (handler request))))
 
 ;; ================================================================================
@@ -34,18 +33,20 @@
 (defn index [csrf-token]
   (log/debug "Serving index.html")
   (html5
-    [:html {:lang "en"}
-     [:head {:lang "en"}
-      [:title "Application"]
-      [:meta {:charset "utf-8"}]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"}]
-      [:link {:href "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
-              :rel  "stylesheet"}]
-      [:link {:rel "shortcut icon" :href "data:image/x-icon;," :type "image/x-icon"}]
-      [:script (str "var fulcro_network_csrf_token = '" csrf-token "';")]]
-     [:body
-      [:div#app]
-      [:script {:src "js/main/main.js"}]]]))
+   [:html {:lang "en"}
+    [:head {:lang "en"}
+     [:title "Application"]
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"}]
+     [:link {:href "https://unpkg.com/react-quill@1.3.3/dist/quill.snow.css"
+             :rel "stylesheet"}]
+     [:link {:href "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+             :rel  "stylesheet"}]
+     [:link {:rel "shortcut icon" :href "data:image/x-icon;," :type "image/x-icon"}]
+     [:script (str "var fulcro_network_csrf_token = '" csrf-token "';")]]
+    [:body
+     [:div#app]
+     [:script {:src "js/main/main.js"}]]]))
 
 ;; ================================================================================
 ;; Workspaces can be accessed via shadow's http server on http://localhost:8023/workspaces.html
@@ -55,30 +56,30 @@
 (defn wslive [csrf-token]
   (log/debug "Serving wslive.html")
   (html5
-    [:html {:lang "en"}
-     [:head {:lang "en"}
-      [:title "devcards"]
-      [:meta {:charset "utf-8"}]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"}]
-      [:link {:href "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
-              :rel  "stylesheet"}]
-      [:link {:rel "shortcut icon" :href "data:image/x-icon;," :type "image/x-icon"}]
-      [:script (str "var fulcro_network_csrf_token = '" csrf-token "';")]]
-     [:body
-      [:div#app]
-      [:script {:src "workspaces/js/main.js"}]]]))
+   [:html {:lang "en"}
+    [:head {:lang "en"}
+     [:title "devcards"]
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"}]
+     [:link {:href "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+             :rel  "stylesheet"}]
+     [:link {:rel "shortcut icon" :href "data:image/x-icon;," :type "image/x-icon"}]
+     [:script (str "var fulcro_network_csrf_token = '" csrf-token "';")]]
+    [:body
+     [:div#app]
+     [:script {:src "workspaces/js/main.js"}]]]))
 
 (defn wrap-html-routes [ring-handler]
   (fn [{:keys [uri anti-forgery-token] :as req}]
     (cond
       (#{"/" "/index.html"} uri)
       (-> (resp/response (index anti-forgery-token))
-        (resp/content-type "text/html"))
+          (resp/content-type "text/html"))
 
       ;; See note above on the `wslive` function.
       (#{"/wslive.html"} uri)
       (-> (resp/response (wslive anti-forgery-token))
-        (resp/content-type "text/html"))
+          (resp/content-type "text/html"))
 
       :else
       (ring-handler req))))
@@ -88,12 +89,12 @@
   (let [defaults-config (:ring.middleware/defaults-config config)
         legal-origins   (get config :legal-origins #{"localhost"})]
     (-> not-found-handler
-      (wrap-api "/api")
-      wrap-transit-params
-      wrap-transit-response
-      (wrap-html-routes)
+        (wrap-api "/api")
+        wrap-transit-params
+        wrap-transit-response
+        (wrap-html-routes)
       ;; If you want to set something like session store, you'd do it against
       ;; the defaults-config here (which comes from an EDN file, so it can't have
       ;; code initialized).
       ;; E.g. (wrap-defaults (assoc-in defaults-config [:session :store] (my-store)))
-      (wrap-defaults defaults-config))))
+        (wrap-defaults defaults-config))))
--- src/main/app/ui/root.cljs
+++ src/main/app/ui/root.cljs
@@ -1,203 +1,117 @@
 (ns app.ui.root
   (:require
-    [app.model.session :as session]
-    [clojure.string :as str]
-    [com.fulcrologic.fulcro.dom :as dom :refer [div ul li p h3 button b]]
-    [com.fulcrologic.fulcro.dom.html-entities :as ent]
-    [com.fulcrologic.fulcro.dom.events :as evt]
-    [com.fulcrologic.fulcro.application :as app]
-    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
-    [com.fulcrologic.fulcro.ui-state-machines :as uism :refer [defstatemachine]]
-    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
-    [com.fulcrologic.fulcro.algorithms.merge :as merge]
-    [com.fulcrologic.fulcro-css.css :as css]
-    [com.fulcrologic.fulcro.algorithms.form-state :as fs]
-    [taoensso.timbre :as log]))
+   ["/app/ui/dividerBlot$default" :as DividerBlot]
+   ["highlight.js" :as highlight]
+   ["quill" :as Quill]
+   ["quill-delta" :as Delta]
+   ["quill-image-compress$default" :as ImageCompress]
+   [app.model.session :as session]
+   [clojure.string :as str]
+   [com.fulcrologic.fulcro-css.css :as css]
+   [com.fulcrologic.fulcro.algorithms.form-state :as fs]
+   [com.fulcrologic.fulcro.algorithms.merge :as merge]
+   [com.fulcrologic.fulcro.application :as app]
+   [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+   [com.fulcrologic.fulcro.dom :as dom :refer [b button div h3 li p ul]]
+   [com.fulcrologic.fulcro.dom.events :as evt]
+   [com.fulcrologic.fulcro.dom.html-entities :as ent]
+   [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
+   [com.fulcrologic.fulcro.react.hooks :as hooks]
+   [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
+   [com.fulcrologic.fulcro.ui-state-machines :as uism :refer [defstatemachine]]
+   [taoensso.timbre :as log]))
 
-(defn field [{:keys [label valid? error-message] :as props}]
-  (let [input-props (-> props (assoc :name label) (dissoc :label :valid? :error-message))]
-    (div :.ui.field
-      (dom/label {:htmlFor label} label)
-      (dom/input input-props)
-      (dom/div :.ui.error.message {:classes [(when valid? "hidden")]}
-        error-message))))
+(def empty-delta "{\"ops\":[{\"insert\":\"\\n\"}]}")
+(def delta-min-size (count empty-delta))
 
-(defsc SignupSuccess [this props]
-  {:query         ['*]
-   :initial-state {}
-   :ident         (fn [] [:component/id :signup-success])
-   :route-segment ["signup-success"]}
-  (div
-    (dom/h3 "Signup Complete!")
-    (dom/p "You can now log in!")))
+(defonce quill-register
+  (doto Quill
+    (.register "modules/imageCompress" ImageCompress)
+    (.register DividerBlot)))
 
-(defsc Signup [this {:account/keys [email password password-again] :as props}]
-  {:query             [:account/email :account/password :account/password-again fs/form-config-join]
-   :initial-state     (fn [_]
-                        (fs/add-form-config Signup
-                          {:account/email          ""
-                           :account/password       ""
-                           :account/password-again ""}))
-   :form-fields       #{:account/email :account/password :account/password-again}
-   :ident             (fn [] session/signup-ident)
-   :route-segment     ["signup"]
-   :componentDidMount (fn [this]
-                        (comp/transact! this [(session/clear-signup-form)]))}
-  (let [submit!  (fn [evt]
-                   (when (or (identical? true evt) (evt/enter-key? evt))
-                     (comp/transact! this [(session/signup! {:email email :password password})])
-                     (log/info "Sign up")))
-        checked? (fs/checked? props)]
-    (div
-      (dom/h3 "Signup")
-      (div :.ui.form {:classes [(when checked? "error")]}
-        (field {:label         "Email"
-                :value         (or email "")
-                :valid?        (session/valid-email? email)
-                :error-message "Must be an email address"
-                :autoComplete  "off"
-                :onKeyDown     submit!
-                :onChange      #(m/set-string! this :account/email :event %)})
-        (field {:label         "Password"
-                :type          "password"
-                :value         (or password "")
-                :valid?        (session/valid-password? password)
-                :error-message "Password must be at least 8 characters."
-                :onKeyDown     submit!
-                :autoComplete  "off"
-                :onChange      #(m/set-string! this :account/password :event %)})
-        (field {:label         "Repeat Password" :type "password" :value (or password-again "")
-                :autoComplete  "off"
-                :valid?        (= password password-again)
-                :error-message "Passwords do not match."
-                :onChange      #(m/set-string! this :account/password-again :event %)})
-        (dom/button :.ui.primary.button {:onClick #(submit! true)}
-          "Sign Up")))))
+(defn insert-into-editor [editor-ref]
+  (fn [image-base64-url image-blob]
+    (let [editor (some-> editor-ref .-current)
+          range (.getSelection editor)]
+      (when range
+        ^js (.insertEmbed editor (.-index range) "image" image-base64-url "user")
+        (set! (.-index range) (inc (.-index range)))
+        (.getSelection editor range "api")))))
 
-(declare Session)
+(defn quill-config [insert-into-editor-with-ref]
+  {:modules {:syntax {:highlight (fn [text language]
+                                   (-> highlight
+                                       (.highlightAuto text)
+                                       (.-value))
+                                            ;; TODO: ideally we use that code when language
+                                            ;; can be specified for a codeblock in quill
+                                   #_(if language
+                                       (-> highlight
+                                           (.highlight text #js {:language language})
+                                           (.-value))
+                                       text))}
+             :toolbar [[{:header []}]
+                       ["bold" "italic" "underline"]
+                       [{:list "bullet"}
+                        {:list "ordered"}]
+                       [{:script "sub"} {:script "super"}]
+                       [{:indent "-1"} {:indent "+1"}]
+                       ["blockquote" "code-block"]
+                       ["link" "image" "formula" "video"]
+                       ["clean"]]
+             :imageCompress {:quality 0.7
+                             :maxWidth 1920
+                             :maxHeight 1920
+                             :mimeType "image/png"
+                             :imageType "image/png"
+                             :debug false
+                             :insertIntoEditor insert-into-editor-with-ref}}
+   :theme "snow"})
 
-(defsc Login [this {:account/keys [email]
-                    :ui/keys      [error open?] :as props}]
-  {:query         [:ui/open? :ui/error :account/email
-                   {[:component/id :session] (comp/get-query Session)}
-                   [::uism/asm-id ::session/session]]
-   :css           [[:.floating-menu {:position "absolute !important"
-                                     :z-index  1000
-                                     :width    "300px"
-                                     :right    "0px"
-                                     :top      "50px"}]]
-   :initial-state {:account/email "" :ui/error ""}
-   :ident         (fn [] [:component/id :login])}
-  (let [current-state (uism/get-active-state this ::session/session)
-        {current-user :account/name} (get props [:component/id :session])
-        initial?      (= :initial current-state)
-        loading?      (= :state/checking-session current-state)
-        logged-in?    (= :state/logged-in current-state)
-        {:keys [floating-menu]} (css/get-classnames Login)
-        password      (or (comp/get-state this :password) "")] ; c.l. state for security
+(defsc Editor [this {:keys [value onChange toolbar] :as props}]
+  {:use-hooks? true}
+  (let [^:js editorElement (hooks/use-ref)
+        ^:js editorInstance (hooks/use-ref)
+        insert-into-editor-with-ref (hooks/use-callback (insert-into-editor editorInstance)
+                                                        [editorInstance])
+        [currentValue setCurrentValue] (hooks/use-state (or value
+                                                            empty-delta))
+        _ (hooks/use-effect
+           (fn []
+             (when-let [editor-element (.-current editorElement)]
+               (let [quill-instance (cond-> (quill-config insert-into-editor-with-ref)
+                                      toolbar (assoc-in [:modules :toolbar] toolbar)
+                                      :always (->> clj->js (Quill. editor-element)))]
+                 (set! (.-current editorInstance) quill-instance)
+                 (.setContents (.-current editorInstance)
+                               (Delta. (.parse js/JSON currentValue)))
+                 (.on (.-current editorInstance) "text-change"
+                      (fn []
+                        (let [delta-str (.stringify js/JSON
+                                                    ^js (.-delta
+                                                         (.-editor
+                                                          (.-current editorInstance))))]
+                          (setCurrentValue delta-str)
+                          (when onChange
+                            (onChange delta-str)))))))
+             (fn []))
+           [editorElement])
+        _ (hooks/use-effect (fn []
+                              (when (not= (js->clj (.parse js/JSON currentValue))
+                                          (js->clj (.parse js/JSON value)))
+                                (setCurrentValue value)
+                                (.setContents (.-current editorInstance)
+                                              (Delta. (.parse js/JSON value))))
+                              (fn []))
+                            [value])]
     (dom/div
-      (when-not initial?
-        (dom/div :.right.menu
-          (if logged-in?
-            (dom/button :.item
-              {:onClick #(uism/trigger! this ::session/session :event/logout)}
-              (dom/span current-user) ent/nbsp "Log out")
-            (dom/div :.item {:style   {:position "relative"}
-                             :onClick #(uism/trigger! this ::session/session :event/toggle-modal)}
-              "Login"
-              (when open?
-                (dom/div :.four.wide.ui.raised.teal.segment {:onClick (fn [e]
-                                                                        ;; Stop bubbling (would trigger the menu toggle)
-                                                                        (evt/stop-propagation! e))
-                                                             :classes [floating-menu]}
-                  (dom/h3 :.ui.header "Login")
-                  (div :.ui.form {:classes [(when (seq error) "error")]}
-                    (field {:label    "Email"
-                            :value    email
-                            :onChange #(m/set-string! this :account/email :event %)})
-                    (field {:label    "Password"
-                            :type     "password"
-                            :value    password
-                            :onChange #(comp/set-state! this {:password (evt/target-value %)})})
-                    (div :.ui.error.message error)
-                    (div :.ui.field
-                      (dom/button :.ui.button
-                        {:onClick (fn [] (uism/trigger! this ::session/session :event/login {:username email
-                                                                                             :password password}))
-                         :classes [(when loading? "loading")]} "Login"))
-                    (div :.ui.message
-                      (dom/p "Don't have an account?")
-                      (dom/a {:onClick (fn []
-                                         (uism/trigger! this ::session/session :event/toggle-modal {})
-                                         (dr/change-route this ["signup"]))}
-                        "Please sign up!"))))))))))))
+     {:className "editor notranslate"}
+     (dom/div {:ref editorElement}))))
 
-(def ui-login (comp/factory Login))
-
-(defsc Main [this props]
-  {:query         [:main/welcome-message]
-   :initial-state {:main/welcome-message "Hi!"}
-   :ident         (fn [] [:component/id :main])
-   :route-segment ["main"]}
-  (div :.ui.container.segment
-    (h3 "Main")
-    (p (str "Welcome to the Fulcro template. "
-         "The Sign up and login functionalities are partially implemented, "
-         "but mostly this is just a blank slate waiting "
-         "for your project."))))
-
-(defsc Settings [this {:keys [:account/time-zone :account/real-name] :as props}]
-  {:query         [:account/time-zone :account/real-name :account/crap]
-   :ident         (fn [] [:component/id :settings])
-   :route-segment ["settings"]
-   :initial-state {}}
-  (div :.ui.container.segment
-    (h3 "Settings")
-    (div "TODO")))
-
-(dr/defrouter TopRouter [this props]
-  {:router-targets [Main Signup SignupSuccess Settings]})
-
-(def ui-top-router (comp/factory TopRouter))
-
-(defsc Session
-  "Session representation. Used primarily for server queries. On-screen representation happens in Login component."
-  [this {:keys [:session/valid? :account/name] :as props}]
-  {:query         [:session/valid? :account/name]
-   :ident         (fn [] [:component/id :session])
-   :pre-merge     (fn [{:keys [data-tree]}]
-                    (merge {:session/valid? false :account/name ""}
-                      data-tree))
-   :initial-state {:session/valid? false :account/name ""}})
-
-(def ui-session (comp/factory Session))
-
-(defsc TopChrome [this {:root/keys [router current-session login]}]
-  {:query         [{:root/router (comp/get-query TopRouter)}
-                   {:root/current-session (comp/get-query Session)}
-                   [::uism/asm-id ::TopRouter]
-                   {:root/login (comp/get-query Login)}]
-   :ident         (fn [] [:component/id :top-chrome])
-   :initial-state {:root/router          {}
-                   :root/login           {}
-                   :root/current-session {}}}
-  (let [current-tab (some-> (dr/current-route this this) first keyword)]
-    (div :.ui.container
-      (div :.ui.secondary.pointing.menu
-        (dom/a :.item {:classes [(when (= :main current-tab) "active")]
-                       :onClick (fn [] (dr/change-route this ["main"]))} "Main")
-        (dom/a :.item {:classes [(when (= :settings current-tab) "active")]
-                       :onClick (fn [] (dr/change-route this ["settings"]))} "Settings")
-        (div :.right.menu
-          (ui-login login)))
-      (div :.ui.grid
-        (div :.ui.row
-          (ui-top-router router))))))
-
-(def ui-top-chrome (comp/factory TopChrome))
+(def editor (comp/factory Editor))
 
 (defsc Root [this {:root/keys [top-chrome]}]
-  {:query         [{:root/top-chrome (comp/get-query TopChrome)}]
-   :initial-state {:root/top-chrome {}}}
-  (ui-top-chrome top-chrome))
+  {}
+  (dom/div
+   (editor {:value "{\"ops\":[{\"insert\":{\"divider\":true}},{\"insert\":\"\\n\"}]}"
+            :onChange (fn [_])})))
